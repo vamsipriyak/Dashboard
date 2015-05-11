aaabfcbb6b5236ff8267eee45b1c9eb914f79038
unknown <m1029090@A4MD11924.mindtree.com>
2015-05-11 11:21:17 +0530

    Fetching parameters from DB

--- HTML/includes/leftpanel.php
+++ HTML/includes/leftpanel.php
@@ -4,32 +4,33 @@ if(empty($_REQUEST['param'])){
 } else {
     $param = $_REQUEST['param'];
 }
+	//Capture pageid passed from home page
+	$pageid=(int)$_REQUEST['pageid'];
+
  ?>
  <!--/. NAV TOP  -->
  
         <nav class="navbar-default navbar-side" role="navigation">
           <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
-
-                    <li>
-                        <a href="performancedetails.php?param=pagespeed" <?php if($param =="" || $param=="pagespeed") { ?> class="active-menu" <?php } ?> > Page Score</a>
-                    </li>
-                    <li>
-                        <a href="performancedetails.php?param=firstbyte" <?php if( $param=="firstbyte") { ?> class="active-menu" <?php } ?> > First Byte</a>
-                    </li>
-					<li>
-                        <a href="performancedetails.php?param=Param 3" <?php if( $param=="Param 3") { ?> class="active-menu" <?php } ?>> Param 3</a>
-                    </li>
-                    <li>
-                        <a href="performancedetails.php?param=Param 4" <?php if( $param=="Param 4") {  ?>class="active-menu" <?php } ?>> Param 4</a>
-                    </li>
-                    
-                    <li>
-                        <a href="performancedetails.php?param=Param 5" <?php if( $param=="Param 5") { ?> class="active-menu" <?php } ?>> Param 5</a>
-                    </li>
-                    <li>
-                        <a href="performancedetails.php?param=Param 6" <?php if( $param=="Param 6") { ?> class="active-menu" <?php } ?>> Param 6 </a>
-                    </li>
+					<?php										
+					   $collection = $db->parameters;
+					   $cursor = $collection->find()->sort(array('_id' => 1));					   
+					   // iterate cursor to display title of documents
+					   foreach ($cursor as $document) {
+						 //If the panel is clicked then include 'active-menu' class.		
+						 if($param == $document['name'])
+						 {
+							$activeMenu =  'class="active-menu"';
+						 } 
+						 else{
+							 $activeMenu =  '';
+						 }
+						print '<li>';
+						print '<a href="performancedetails.php?pageid='.$pageid.'&param='.$document['_id'].'" '.$activeMenu.' >'.$document['name'].'</a>';
+						print '</li>';
+					   }
+					?>										
                 </ul>
 
             </div>
