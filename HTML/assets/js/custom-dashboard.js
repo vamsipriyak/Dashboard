d2f01cd266dde7da38551552b23448f70cc7e686
unknown <m1029090@A4MD11924.mindtree.com>
2015-05-22 09:02:32 +0530

    Added validation for adding web pages

--- HTML/assets/js/custom-dashboard.js
+++ HTML/assets/js/custom-dashboard.js
@@ -13,6 +13,13 @@
 		function postWebsiteParams() {
 			var webPageUrl = document.getElementById("webPageUrl").value;
 			var parentSiteId = document.getElementById("parentSiteId").value;
+			var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+			isValid = regexp.test(webPageUrl);
+			if(!isValid)
+			{
+				alert('Please enter a valid URL');
+				return;						
+			}				
 			// Returns successful data submission message when the entered information is stored in database.
 			var dataString = "webPageUrl=" + webPageUrl + "&parentSiteId=" + parentSiteId;
 			//alert(dataString);
