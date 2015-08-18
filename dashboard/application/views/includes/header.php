714f0b2b9a347b3a7f7f4f966f3ccf9dffbd7dd3
senthilkanna <senkanna.mca@gmail.com>
2015-08-18 14:59:08 +0530

    changes

--- dashboard/application/views/includes/header.php
+++ dashboard/application/views/includes/header.php
@@ -45,8 +45,12 @@
 			?>
               <div style="float:right" >
 			  <?php if (strpos($url,'/help') == false) { ?>
+			    <?php if (strpos($url,'/Group') == false) { ?>
+			 
+			    <a href="<?php echo $this->config->base_url(); ?>index.php/Group" class="navbar-help">Add a site </a> 
+				<?php   } ?> 
 			  <?php if (strpos($url,'/form') == false) { ?>
-			   <a href="<?php echo $this->config->base_url(); ?>index.php/Group" class="navbar-help">Add a site </a> 
+			 
 			  <a href="<?php echo $this->config->base_url(); ?>index.php/form" class="navbar-help">Add a webpage </a> | <?php   } ?> 
 			  <?php if (strpos($url,'/edit') == false) { ?>
 			  <a href="<?php echo $this->config->base_url(); ?>index.php/edit" class="navbar-help">Edit Parameters </a> 
